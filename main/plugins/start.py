f6cd9b0c5b088d697ffc36f14fca01972a9806e2
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-05-02 13:57:39 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -42,7 +42,7 @@ async def info(event):
                          Button.inline("Menu", data="menu")]
                          [
                          Button.inline("Updates", url="https://t.me/REX_BOTZ"),
-                         Button.inline(Support", url="https://t.me/REX_Bots_Support")])
+                         Button.inline("Support", url="https://t.me/REX_Bots_Support")])
     
 @Drone.on(events.callbackquery.CallbackQuery(data="notice"))
 async def notice(event):
