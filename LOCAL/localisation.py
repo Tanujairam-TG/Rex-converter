21d6c77146c793d866b5bbffd672be27a3b379d8
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-05-02 15:21:19 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,17 +1,17 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Hello there \n  Send me any file to begin."
+START_TEXT = "Hello there \n Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/REX_BOTZ"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/REX_Bots_Support"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by team rex"   
 
-help_text = """**v1.4**
+help_text = """
 
 •`Encode` - encode your video into different lib format or resolution
 
@@ -29,7 +29,7 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/Tanujairam"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
@@ -41,5 +41,5 @@ JPG2 = "LOCAL/20211215_165751.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
 
-JPG4 = "LOCAL/20211219_000258.jpg"
+JPG = "LOCAL/20211219_000258.jpg"
 
